48ac16047e35a5a7a8f5a4c4b1624ac35677d343
Hyperboloider <kknyaz033@gmail.com>
2021-02-09 20:37:21 +0200

    interactive input and csv parser functions added

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import os
+from classes import Team
 
-directory = "D:/homework2/lab2/csvs/"
-files = os.listdir(directory)
-csvs = []
-teams = []
+
+def path_input():
+    choice = input('What kind of path you want to insert?\nRelative-"r", Absolute-"a":\n')
+    if choice == 'r':
+        directory = input('Insert the directory name: ')
+        path = os.getcwd() + '\\' + directory +'\\'
+        return path
+    elif choice == 'a':
+        path = input('Insert the path: ')
+        return path
+    else:
+        print("unsupported choice")
 
 def find_csv(directory):
     csvs = []
@@ -12,13 +21,18 @@ def find_csv(directory):
             csvs.append(f)
     return csvs
 
+def team_creator(directory, csvs, teams):
+    for csv in csvs:
+        with open(directory + csv, 'r') as table:
+            numberOfRows = int(table.readline())
+            for _ in range(numberOfRows):
+                teams.append(Team(table.readline()))
+
+directory = path_input()
+files = os.listdir(directory)
+teams = []
 csvs = find_csv(directory)
+team_creator(directory, csvs, teams)
 
-print(csvs)
-
-for csv in csvs:
-    with open(directory + csv, 'r') as table:
-        numberOfRows = int(table.readline())
-        for _ in range(numberOfRows):
-            print(table.readline(), end='')
-        print('\n')
+for team in teams:
+    print(team.name, team.points)
